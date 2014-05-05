ffe2ca68a7e628e51b25ff9bc79a892700e62957
hguerrero <hguerrero@hotmail.com>
2014-05-05 15:28:47 -0500

    create database schema

--- src/main/resources/initial_data.sql
+++ src/main/resources/initial_data.sql
@@ -1,3 +1,4 @@
+CREATE SCHEMA denunciame;
 insert into denunciame.type (id, description, status) values (1, 'homicidio', 1);
 insert into denunciame.type (id, description, status) values (2, 'genocidio', 1);
 insert into denunciame.type (id, description, status) values (3, 'asalto', 1);
