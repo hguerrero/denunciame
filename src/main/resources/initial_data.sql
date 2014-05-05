b08c81692905bb1f40071368c58534e52b68b14f
hguerrero <hguerrero@hotmail.com>
2014-05-05 15:49:27 -0500

    Restore initial_data.sql

--- src/main/resources/initial_data.sql
+++ src/main/resources/initial_data.sql
@@ -1,4 +1,3 @@
-CREATE SCHEMA denunciame;
 insert into denunciame.type (id, description, status) values (1, 'homicidio', 1);
 insert into denunciame.type (id, description, status) values (2, 'genocidio', 1);
 insert into denunciame.type (id, description, status) values (3, 'asalto', 1);
